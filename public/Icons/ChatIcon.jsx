dde5e31eaae250d334917164326ed8d08fb2e0f5
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-24 03:45:07 +0530

    chatIcon

---
+++ public/Icons/ChatIcon.jsx
@@ -0,0 +1,21 @@
+export default function ChatIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-5 w-5"
+      >
+        <path
+          d="M21 11.5c0 4.42-4.03 8-9 8-1.54 0-2.98-.31-4.25-.88l-3.75 1.13 1.12-3.74C3.31 14.5 3 13.03 3 11.5 3 7.08 7.03 3.5 12 3.5s9 3.58 9 8Z"
+        />
+      </svg>
+    );
+  }
+  
