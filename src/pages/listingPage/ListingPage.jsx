1cb6b4b9d22c0bf0dcef919d1f5ac388337df77e
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-24 21:11:08 +0530

    initialising the listing page

---
+++ src/pages/listingPage/ListingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../listingPage/listingPage.scss"
+
+function ListingPage() {
+  return (
+    <div>
+        <h1>Listing</h1>
+    </div>
+  )
+}
+
+export default ListingPage
