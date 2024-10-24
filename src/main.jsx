f82521a3395f485ad96fb77f00c3ba37c43bff8d
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-24 21:10:34 +0530

    integrating react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./index.scss"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import "./pages/layoutPage/Layout.jsx"
+import Layout from './pages/layoutPage/Layout.jsx';
+import HomePage from './pages/homePage/homePage.jsx';
+import ListingPage from './pages/listingPage/ListingPage.jsx';
+
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<HomePage/>}/>
+      <Route path='list' element={<ListingPage/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>,
 )
