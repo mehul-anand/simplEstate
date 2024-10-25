533870e19ec1ea5002debfe8111b64bc8f6de850
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-26 01:37:19 +0530

    pin component

---
+++ src/components/pin/Pin.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Marker, Popup } from "react-leaflet";
+import { Link } from "react-router-dom";
+import "./pin.scss"
+
+function Pin({ item }) {
+  return (
+    <div className="pin">
+      <Marker position={[item.latitude, item.longitude]}>
+        <Popup>
+          <div className="popupContainer">
+            <img src={item.img} alt={item.title} />
+            <div className="textContainer">
+              <Link to={`/${item.id}`}>{item.title}</Link>
+              <span>
+                {item.bedroom}{" "}
+                {item.bedroom > 1 ? "bedrooms" : "bedroom"}
+              </span>
+              <span>
+                {item.bathroom}{" "}
+                {item.bathroom > 1 ? "bathrooms" : "bathroom"}
+              </span>
+              <b>₹ {item.price}</b>
+            </div>
+          </div>
+        </Popup>
+      </Marker>
+    </div>
+  );
+}
+
+export default Pin;
