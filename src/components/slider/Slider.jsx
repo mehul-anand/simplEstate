ea4d049d3e234c99ae91da5cecf8ca480230229a
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-26 05:23:06 +0530

    implementing sliding with arrows and keys , added esc key functionality

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,27 +1,98 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./slider.scss";
 
 function Slider({ images }) {
+  const [imageIndex, setImageIndex] = useState(null);
+
+  const changeSlides = (direction) => {
+    if (direction === "right") {
+      if (imageIndex < 3) {
+        setImageIndex((prev) => setImageIndex(prev + 1));
+      } else {
+        setImageIndex(0);
+      }
+    } else {
+      if (imageIndex > 0) {
+        setImageIndex((prev) => setImageIndex(prev - 1));
+      } else {
+        setImageIndex(3);
+      }
+    }
+  };
+
+  const handleEscapeKey = (event) => {
+    if (event.key === "Escape") {
+      setImageIndex(null);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowRight":
+        changeSlides("right");
+        break;
+      case "ArrowLeft":
+        changeSlides("left");
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleEscapeKey);
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [handleEscapeKey]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [handleKeyDown]);
+
   return (
     <div className="slider">
-      <div className="fullSlider">
-        <div className="arrow">
-          <img src="/arrow.png" alt="" />
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow">
+            <img
+              src="/arrow.png"
+              alt="left-arrow"
+              onClick={() => changeSlides("left")}
+            />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow">
+            <img
+              src="/arrow.png"
+              className="rightArrow"
+              alt="right-arrow"
+              onClick={() => changeSlides("right")}
+            />
+          </div>
+          <div className="close" onClick={() => setImageIndex(null)}>
+            X
+          </div>
         </div>
-        <div className="imgContainer">
-          <img src={images[0]} alt="" />
-        </div>
-        <div className="arrow">
-          <img src="/arrow.png" className="rightArrow" alt="" />
-        </div>
-        <div className="close">X</div>
-      </div>
+      )}
       <div className="biggerImg">
-        <img src={images[0]} alt="primary-img" />
+        <img
+          src={images[0]}
+          alt="primary-img"
+          onClick={() => setImageIndex(0)}
+        />
       </div>
       <div className="smallerImgs">
-        {images.slice(1).map((imageToDisp,index) => (
-          <img key={index} src={imageToDisp} alt="supplementary image"/>
+        {images.slice(1).map((imageToDisp, index) => (
+          <img
+            key={index}
+            src={imageToDisp}
+            alt="supplementary image"
+            onClick={() => setImageIndex(index + 1)}
+          />
         ))}
       </div>
     </div>
