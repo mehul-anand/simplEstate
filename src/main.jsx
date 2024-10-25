0c2cc8a53bb048ee31f6d318a71ebc82c441f65a
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-26 04:15:43 +0530

    adding single page route

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,14 @@ import "./pages/layoutPage/Layout.jsx"
 import Layout from './pages/layoutPage/Layout.jsx';
 import HomePage from './pages/homePage/homePage.jsx';
 import ListingPage from './pages/listingPage/ListingPage.jsx';
+import SinglePage from './pages/singlePage/singlePage.jsx';
 
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
       <Route path='' element={<HomePage/>}/>
       <Route path='list' element={<ListingPage/>}/>
+      <Route path= "/:id" element={<SinglePage/>}/>
     </Route>
   )
 )
