f8a169f8e8769273e6b4bce0635cfd831b3aa767
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-10-31 02:50:15 +0530

    adding mock information

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [open,setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [notification,setNotification] = useState(3)
+  const user = true;
   return (
     <div>
       <nav>
@@ -17,14 +20,34 @@ function Navbar() {
           <a href="">Agents</a>
         </div>
         <div className="right">
-          <a href="">Sign In</a>
-          <a href="" className="register">
-            Sign Up
-          </a>
+          {user ? (
+            <div>
+            <div className="user">
+              <img src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="user-img" />
+              <span>User</span>
+              <Link className="profile" to={"/profile"}>
+              <div>
+                <span className="notification">{notification}</span>
+                <span>Profile</span>
+                </div></Link>
+            </div>
+            </div>
+          ) : (
+            <div>
+              <a href="">Sign In</a>
+              <a href="" className="register">
+                Sign Up
+              </a>
+            </div>
+          )}
           <div className="menuIcon">
-            <img src="/menu.png" alt="menu" onClick={()=>setOpen(prev=>!prev)}/>
+            <img
+              src="/menu.png"
+              alt="menu"
+              onClick={() => setOpen((prev) => !prev)}
+            />
           </div>
-          <div className={open?"menu active":"menu"}>
+          <div className={open ? "menu active" : "menu"}>
             <a href="">Home</a>
             <a href="">About</a>
             <a href="">Contact</a>
