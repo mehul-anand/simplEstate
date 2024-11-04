11c70b10e82b4ffe68d7cf21f8d3c08f29e45b28
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-04 10:40:23 +0530

    adding express and routes

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
 const app = express();
-app.use(express.json)
+app.use(express.json()); // Fixed here
 
-app.use("/api/post",postRoute)
-app.use("/api/auth",authRoute)
+app.use("/api/post", postRoute);
+app.use("/api/auth", authRoute);
 
 app.use("/", (req, res) => {
   res.send("main page");
