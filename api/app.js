16683ac649d09be1f7a526e311b27176bff61c18
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-09 14:45:58 +0530

    integrating cookie-parser

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json()); // Fixed here
+app.use(cookieParser())
 
 app.use("/api/post", postRoute);
 app.use("/api/auth", authRoute);
