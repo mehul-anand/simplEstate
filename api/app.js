e3660f084b414272b892d59195053f9a971f2069
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-10 04:54:44 +0530

    cors and testroutes

--- api/app.js
+++ api/app.js
@@ -1,14 +1,19 @@
 import express from "express";
+import cors from "cors"
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(express.json()); // Fixed here
 app.use(cookieParser())
 
 app.use("/api/post", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", authRoute);
 
 app.use("/", (req, res) => {
   res.send("main page");
