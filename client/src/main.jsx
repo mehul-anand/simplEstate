91b9deb6aaa1529e71842378060e2ef746e87a06
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-10 04:56:00 +0530

    register and ligin page integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,6 +15,8 @@ import HomePage from './pages/homePage/homePage.jsx';
 import ListingPage from './pages/listingPage/ListingPage.jsx';
 import SinglePage from './pages/singlePage/singlePage.jsx';
 import ProfilePage from "./pages/profilePage/ProfilePage.jsx"
+import RegisterPage from './pages/registerPage/RegisterPage.jsx';
+import LoginPage from './pages/loginPage/LoginPage.jsx';
 
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +25,8 @@ const appRouter = createBrowserRouter(
       <Route path='list' element={<ListingPage/>}/>
       <Route path= "/:id" element={<SinglePage/>}/>
       <Route path= "/profile" element={<ProfilePage/>}/>
+      <Route path= "/register" element={<RegisterPage/>}/>
+      <Route path= "/login" element={<LoginPage/>}/>
     </Route>
   )
 )
