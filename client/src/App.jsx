7d06b5d291fff025f334f99b6a8e6cc0f7c19f50
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-10 04:55:43 +0530

    removing unecessary code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,3 @@
-import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./pages/homePage/homePage";
-import { RouterProvider ,createBrowserRouter , Route , Link } from "react-router-dom";
-
-// DO ROUTER LIKE HC AND PORTFOLIO
-
 function App() {
   return (
     <>
