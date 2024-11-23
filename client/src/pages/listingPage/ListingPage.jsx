bc8f970437f4a8f03dd08ed15a70086ef5610f59
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:26:56 +0530

    listing page

--- client/src/pages/listingPage/ListingPage.jsx
+++ client/src/pages/listingPage/ListingPage.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./listingPage.scss";
-import { listData } from "../../lib/dummyData";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
+import { Await, useLoaderData } from "react-router-dom";
 
 function ListingPage() {
-  const data = listData;
+  const data = useLoaderData();
+
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
-          {data.map((propertyData) => (
-            <Card key={propertyData.id} propertyData={propertyData} />
-          ))}
+          <Suspense fallback={<p>Loading...</p>}>
+            <Await
+              resolve={data.postResponse}
+              errorElement={<p>No posts yet</p>}
+              // errorElement={<p>Error loading posts!</p>}
+            >
+              {(postResponse) =>
+                postResponse.data.map((post) => (
+                  <Card key={post.id} propertyData={post} />
+                ))
+              }
+            </Await>
+          </Suspense>
         </div>
       </div>
       <div className="mapContainer">
-        <Map items={data}/>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Await
+            resolve={data.postResponse}
+            // errorElement={<p>Error loading posts!</p>}
+            errorElement={<p>No posts yet</p>}
+          >
+            {(postResponse) => <Map items={postResponse.data} />}
+          </Await>
+        </Suspense>
       </div>
     </div>
   );
