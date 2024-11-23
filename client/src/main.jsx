9348f6e22b25553ab28d2360f6344bc2e012387b
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:21:29 +0530

    routing patters

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import "./index.scss";
 import {
   createBrowserRouter,
@@ -10,36 +10,50 @@ import {
 } from "react-router-dom";
 
 import "./pages/layoutPage/Layout.jsx";
-import { Layout, RequireAuth } from './pages/layoutPage/Layout.jsx';
-import HomePage from './pages/homePage/HomePage.jsx';
-import ListingPage from './pages/listingPage/ListingPage.jsx';
-import SinglePage from './pages/singlePage/SinglePage.jsx';
+import { Layout, RequireAuth } from "./pages/layoutPage/Layout.jsx";
+import HomePage from "./pages/homePage/HomePage.jsx";
+import ListingPage from "./pages/listingPage/ListingPage.jsx";
+import SinglePage from "./pages/singlePage/SinglePage.jsx";
 import ProfilePage from "./pages/profilePage/ProfilePage.jsx";
-import RegisterPage from './pages/registerPage/RegisterPage.jsx';
-import LoginPage from './pages/loginPage/LoginPage.jsx';
-import ProfileUpdatePage from './pages/profileUpdatePage/ProfileUpdatePage.jsx';
-import { AuthContextProvider } from './contexts/AuthContext.jsx';
+import RegisterPage from "./pages/registerPage/RegisterPage.jsx";
+import LoginPage from "./pages/loginPage/LoginPage.jsx";
+import ProfileUpdatePage from "./pages/profileUpdatePage/ProfileUpdatePage.jsx";
+import NewPostPage from "./pages/newPostPage/NewPostPage.jsx";
+import { AuthContextProvider } from "./contexts/AuthContext.jsx";
+import { SocketContextProvider } from "./contexts/SocketContext.jsx";
+import {
+  singlePageLoader,
+  listPageLoader,
+  profilePageLoader,
+} from "./lib/loaders.js";
 
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<HomePage />} />
-      <Route path='list' element={<ListingPage />} />
-      <Route path='/:id' element={<SinglePage />} />
-      <Route path='/register' element={<RegisterPage />} />
-      <Route path='/login' element={<LoginPage />} />
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<HomePage />} />
+      <Route path="list" element={<ListingPage />} loader={listPageLoader} />
+      <Route path="/:id" element={<SinglePage />} loader={singlePageLoader} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route element={<RequireAuth />}>
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/profile/update' element={<ProfileUpdatePage />} />
+        <Route
+          path="/profile"
+          element={<ProfilePage />}
+          loader={profilePageLoader}
+        />
+        <Route path="/profile/update" element={<ProfileUpdatePage />} />
+        <Route path="/add" element={<NewPostPage />} />
       </Route>
     </Route>
   )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <RouterProvider router={appRouter} />
+      <SocketContextProvider>
+        <RouterProvider router={appRouter} />
+      </SocketContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
