5f1d51a711b11911c567accd0e3482d86429b692
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:23:21 +0530

    notification navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,64 +1,65 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { useNotificationStore } from "../../lib/notificationStore";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
-  const [notification, setNotification] = useState(3);
+
   const { currentUser } = useContext(AuthContext);
+
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+  if (currentUser) fetch();
+
   return (
-    <div>
-      <nav>
-        <div className="left">
-          <a href="/" className="logo">
-            <img src="/logo.png" alt="logo" />
-            <span>SimplEstate</span>
-          </a>
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>SimplEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/list">Listings</a>
+      </div>
+      <div className="right">
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || "/emptyPfp.jpg"} alt="" />
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              {number > 0 && <div className="notification">{number}</div>}
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <div className="user">
+            {" "}
+            <a href="/login">Sign in</a>
+            <a href="/register" className="register">
+              Sign up
+            </a>
+          </div>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
           <a href="/">Home</a>
-          <a href="/list">Listings</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
         </div>
-        <div className="right">
-          {currentUser ? (
-            <div>
-              <div className="user">
-                <img
-                  src={currentUser.avatar || "./emptyPfp.jpg"}
-                  alt="user-img"
-                />
-                <span>{currentUser.username}</span>
-                <Link className="profile" to={"/profile"}>
-                  <div>
-                    <span className="notification">{notification}</span>
-                    <span>Profile</span>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          ) : (
-            <div className="user">
-              <a href="/login">Sign In</a>
-              <a href="/register" className="register">
-                Sign Up
-              </a>
-            </div>
-          )}
-          <div className="menuIcon">
-            <img
-              src="/menu.png"
-              alt="menu"
-              onClick={() => setOpen((prev) => !prev)}
-            />
-          </div>
-          <div className={open ? "menu active" : "menu"}>
-            <a href="">Home</a>
-            <a href="/list">Listings</a>
-            <a href="/login">Sign In</a>
-            <a href="/register">Sign Up</a>
-          </div>
-        </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
