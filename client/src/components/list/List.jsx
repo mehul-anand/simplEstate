5e816ca4d04f79b6fb803fac35152ed5037c7ca7
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:22:51 +0530

    listing

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./list.scss";
-import { listData } from "../../lib/dummyData";
-import Card from "../card/Card";
+import './list.scss'
+import Card from"../card/Card"
 
-function List() {
-    
+function List({ posts }) {
   return (
     <div className="list">
-      {listData.map((propObj) => (
-        <Card key={propObj.id} propertyData={ propObj } />
-      ))}
+      {posts && posts.length > 0 ? (
+        posts.map((post) => <Card key={post.id} propertyData={post} />)
+      ) : (
+        // <p>No posts available</p>
+        <p>No posts made</p>
+      )}
     </div>
   );
 }
