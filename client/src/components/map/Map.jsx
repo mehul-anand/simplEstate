8985c6b308d311c4e2ca07df9d98433db09cfd1c
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:23:07 +0530

    map component zoom and view adjustments

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import "./map.scss";
-import { MapContainer, TileLayer} from "react-leaflet";
-import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import Pin from "../pin/Pin";
 
-function Map({items}) {
+function Map({ items }) {
+  // console.log(items)
+  const avgValueLatitude  = items.reduce((acc,curr)=>{
+    return acc+Number(curr.latitude)
+  },0)/items.length
+  const avgValueLongitude  = items.reduce((acc,curr)=>{
+    return acc+Number(curr.longitude)
+  },0)/items.length
+
+  // console.log(`avg latitude : ${avgValueLatitude}`)
+  // console.log(`avg latitude : ${avgValueLongitude}`)
   return (
-    <MapContainer className="map" center={[52.4797,-1.90269]} zoom={7} scrollWheelZoom={true}>
+    <MapContainer
+      className="map"
+      center={
+        items.length === 1
+          ? [items[0].latitude, items[0].longitude]
+          : [avgValueLatitude,avgValueLongitude]
+      }
+      zoom={10}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {items.map((item)=>(<Pin key={item.id} item={item}/>))}
+      {items.map((item) => (
+        <Pin key={item.id} item={item} />
+      ))}
     </MapContainer>
   );
 }
