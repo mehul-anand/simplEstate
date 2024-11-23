cd43d395883ef88b6372271dc67e185c1d4e5ba3
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:19:08 +0530

    route integration

--- api/app.js
+++ api/app.js
@@ -3,6 +3,9 @@ import cors from "cors"
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js"
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -11,9 +14,12 @@ app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use(express.json()); // Fixed here
 app.use(cookieParser())
 
-app.use("/api/post", postRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
+app.use("/api/user", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.use("/", (req, res) => {
   res.send("main page");
