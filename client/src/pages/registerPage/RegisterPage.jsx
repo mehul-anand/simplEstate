bf5cc1436d4727a999fbbff8c5987566efe8cf6b
Mehul Anand <89949898+mehul-anand@users.noreply.github.com>
2024-11-24 04:28:02 +0530

    secure register page

--- client/src/pages/registerPage/RegisterPage.jsx
+++ client/src/pages/registerPage/RegisterPage.jsx
@@ -1,12 +1,14 @@
 import "./registerPage.scss";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function RegisterPage() {
   const [errorMessage,setErrorMessage] = useState("")
   const [isLoading,setIsLoading] = useState(false)
   const navigate = useNavigate()
+  const {updateUser} = useContext(AuthContext)
   const handleSubmit = async (e)=>{
     e.preventDefault()
     setErrorMessage("")
@@ -17,9 +19,12 @@ function RegisterPage() {
     const password = formData.get("password")
 
     try {
+      console.log("try")
       const res = await apiRequest.post("auth/register",{username,email,password}) 
+      updateUser(res.data)
       navigate("/login")
     } catch (error) {
+      console.log(`error :${error.response.data.message}`)
       setErrorMessage(error.response.data.message)
     }finally{
       setIsLoading(false)
@@ -31,9 +36,9 @@ function RegisterPage() {
       <div className="formContainer">
         <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="email" type="text" placeholder="Email" />
-          <input name="password" type="password" placeholder="Password" />
+          <input required name="username" type="text" placeholder="Username" />
+          <input required name="email" type="text" placeholder="Email" />
+          <input required name="password" type="password" placeholder="Password" />
           <button disabled={isLoading}>Register</button>
           {errorMessage && <span>{errorMessage}</span>}
           <Link to="/login">Do you have an account?</Link>
